cfe6ba2a2346db7eec6f370bc9c336f34b43f9b1
MUSITELLI <alessandro.musitelli@intesasanpaolo.com>
2023-10-17 00:03:55 +0200

    add entrypoint, dbt run->compile

--- dbt_prj/server.py
+++ dbt_prj/server.py
@@ -6,14 +6,21 @@ dbt = dbtRunner()
 
 lst_cmd = [
     'deps'
-    'run',
+    'compile',
     'docs generate'
 ]
 
+def dbt_cmd(str_cmd: str) -> dbtRunnerResult:
+    return dbt.invoke(str_cmd.split(' '))
+
 for _ in lst_cmd:
-    res: dbtRunnerResult = dbt.invoke(_.split(' '))
+    dbt_cmd(_)
 
 app = FastAPI()
+
+@app.get("/")
+def get_homepage():
+    return dict(status='welcome page')
 
 app.mount(
     "/dbt_docs", # --> sub-path of the "sub-app" will be mounted on
@@ -21,6 +28,7 @@ app.mount(
         directory="target", # name of the dir that contains the static files
         html=True), # --> `Starlette`: Automatically loads index.html for directories if such file exist.
     name="static") # --> used internally by fastapi
+
 
 
 # inspect the results
